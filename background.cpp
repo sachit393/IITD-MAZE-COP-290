6e6f202730518e4dabef110771aff19803854b2a
Eshan Jain <77990628+eshan-292@users.noreply.github.com>
2022-04-04 20:55:34 +0530

    Update background.cpp
    Added text

--- background.cpp
+++ background.cpp
@@ -1,15 +1,13 @@
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 #include <stdio.h>
 #include <string>
 
+const int HOSPITAL_BILL = 90;
 
-const int SCREEN_WIDTH = 1520;
-const int SCREEN_HEIGHT = 1020;
-
-//The window we'll be rendering to
+const int PLAYER_RADIUS = 10;
 SDL_Window* gWindow = NULL;
 
 //The window renderer
@@ -17,6 +15,16 @@ SDL_Renderer* gRenderer = NULL;
 
 //Current displayed texture
 SDL_Texture* gTexture = NULL;
+enum KeyPress
+{
+    KEY_PRESS_UP,
+    KEY_PRESS_DOWN,
+    KEY_PRESS_LEFT,
+    KEY_PRESS_RIGHT,
+};
+
+
+
 
 TTF_Font* font;
 
@@ -68,6 +76,154 @@ void disptext(SDL_Renderer* gRenderer, int x, int y, int w, int h, char* s){
 
 
 }
+
+
+
+
+
+void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius)
+{
+   const int32_t diameter = (radius * 2);
+
+   int32_t x = (radius - 1);
+   int32_t y = 0;
+   int32_t tx = 1;
+   int32_t ty = 1;
+   int32_t error = (tx - diameter);
+
+   while (x >= y)
+   {
+      //  Each of the following renders an octant of the circle
+      SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+      SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+      SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+      SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+      SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+      SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+      SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+      SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+      if (error <= 0)
+      {
+         ++y;
+         error += ty;
+         ty += 2;
+      }
+
+      if (error > 0)
+      {
+         --x;
+         tx += 2;
+         error += (tx - diameter);
+      }
+   }
+}
+
+class Player{
+
+        public:
+                int x;
+                int y;
+                int speed = 2;
+                int health;
+                int energy;
+                int happiness;
+                int money;
+                int knowledge;
+                bool hasCycle;
+        Player(int xP,int yP){
+                health = 50;
+                energy = 100;
+                happiness = 50;
+                money = 100;
+                knowledge = 10;
+                hasCycle = false;
+                x = xP;
+                y = yP;
+        }
+
+
+        void buyCycleIfPossible(){
+                if(money>200 && !hasCycle){
+                        money-=200;
+                        energy*=2;
+                        hasCycle = true;
+
+                }
+                
+        }
+
+        void hospitalize(){
+                money-=HOSPITAL_BILL;
+                energy = 100;
+                health = 100;
+                happiness = 50;
+        }
+
+        void changeEnergy(int amount){
+                energy+=amount;
+                if(energy<0){
+                        hospitalize();
+                }
+        }
+
+        void changeHealth(int amount){
+                health+=amount;
+                if(health<0){
+                        hospitalize();
+                }
+        }
+
+        void changeHappiness(int amount){
+                happiness+=amount;
+
+                if(happiness<0){                // permanent decrease in energy due to lack of happiness
+                        energy/=2;
+                        happiness = 0;
+                }
+
+        }
+
+        void changeKnowledge(int amount){
+                knowledge+=amount;
+        }
+        void updateSpeed(){
+                speed = 2+energy/10;
+        }
+
+        void move(int keyPress){
+                if(keyPress == KEY_PRESS_UP){
+                        y-=speed;
+                }
+                if(keyPress == KEY_PRESS_DOWN){
+                        y+=speed;
+                }
+                if(keyPress == KEY_PRESS_LEFT){
+                        x-=speed;
+                }
+                if(keyPress == KEY_PRESS_RIGHT){
+                        x+=speed;
+                }
+        }
+
+        void renderPlayer(){
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS);
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS-1);
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS-2);
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS-3);
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS-4);
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS-5);
+                DrawCircle(gRenderer,x,y,PLAYER_RADIUS-6);
+        }
+
+};
+
+const int SCREEN_WIDTH = 1520;
+const int SCREEN_HEIGHT = 1020;
+
+//The window we'll be rendering to
+
+
 
 
 bool init(){
@@ -140,6 +296,7 @@ void close()
 
 int main(int argc, char const *argv[])
 {
+	
 	if (!init()){
 		printf ("failed to initialize\n");
 	}
@@ -485,6 +642,63 @@ int main(int argc, char const *argv[])
 
             
             disptext(gRenderer,  SCREEN_WIDTH / 22+220+2*SCREEN_WIDTH / 12, SCREEN_HEIGHT / 22+540 + SCREEN_HEIGHT / 4+ SCREEN_HEIGHT / 16+40, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 12 , "Main Building" ) ;
+
+
+
+            SDL_RenderPresent(gRenderer);                     
+
+		}
+
+	}
+
+
+
+///////////////////////////////////////////////////////////////////
+
+
+            // SDL_Surface* textSurface = TTF_RenderText_Shaded(font, totalKilled,    foregroundColor, backgroundColor);
+
+
+            SDL_SetRenderDrawColor( gRenderer,235, 52, 155, 0xFF );
+            player1.renderPlayer();
+
+
+            SDL_SetRenderDrawColor( gRenderer,92, 52, 235, 0xFF );
+            player2.renderPlayer();
+
+
+            while( SDL_PollEvent( &e ) != 0 )
+                {
+                        //User requests quit
+                        if( e.type == SDL_QUIT )
+                        {
+                                quit = true;
+                        }
+                        else if(e.type == SDL_KEYDOWN){
+                                switch (e.key.keysym.sym){
+                                        case SDLK_UP:
+                                                player1.move(KEY_PRESS_UP);
+                                                break;
+                                        case SDLK_DOWN:
+                                                player1.move(KEY_PRESS_DOWN);
+                                                break;
+                                        case SDLK_LEFT:
+                                                player1.move(KEY_PRESS_LEFT);
+                                                break;
+                                        case SDLK_RIGHT:
+                                                player1.move(KEY_PRESS_RIGHT);
+                                                break;
+                                }
+                        }
+                        SDL_SetRenderDrawColor( gRenderer,235, 52, 155, 0xFF );
+                        player1.renderPlayer();
+                }
+
+
+
+
+
+
 
 
 
